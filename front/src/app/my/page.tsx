89139fed73fc627d3a4d8883232fce2f5ef82a30
박가영 <ppoppo1217@naver.com>
2025-04-05 14:33:45 +0900

    feat: 마이페이지 구현
    관심제품, 리뷰 미리보기 포함해서 마이페이지 구현
    fetchWrapper 미리 적용
    
    S12P21A507-131
    S12P21A507-132

--- front/src/app/my/page.tsx
+++ front/src/app/my/page.tsx
@@ -1,13 +1,34 @@
 "use client";
 import React from "react";
+import ReviewUserInfo from "../_components/user/UserInfo";
+import { useUserStore } from "@/stores/UserStore";
+import UserWordCloud from "../_components/wordcloud/UserWordCloud";
+import PreviewLikeItems from "./_components/PreviewLikeItems";
+import PreviewMyReview from "./_components/PreviewMyReview";
+import B5 from "../_components/common/B5";
+import { useRouter } from "next/navigation";
 
 export default function MyPage() {
+  const { user, keywords } = useUserStore();
+  const router = useRouter();
+
+  const handleClickWithdraw = () => {
+    router.push("/my/withdraw");
+  };
+
   return (
-    <div>
-      <div className="text-head0 font-title text-pink1">마이 페이지입니다.</div>
-      <a href="/" className="text-size2 text-brown1">
-        Landing으로 이동
-      </a>
+    <div className="flex flex-col gap-2 pt-[2rem]">
+      <ReviewUserInfo user={user} />
+      <UserWordCloud words={keywords} />
+      <PreviewLikeItems />
+      <PreviewMyReview user={user} />
+      <div className="flex flex-row items-center justify-center gap-3 text-gray3">
+        <B5
+          children={"로그아웃"}
+          onClick={() => console.log("로그아웃 api 붙이기")}
+        />
+        <B5 children={"회원탈퇴"} onClick={() => handleClickWithdraw()} />
+      </div>
     </div>
   );
 }
