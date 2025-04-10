863a0b089b0fac083ef5af384e6430f2e1026451
최은영 <harrysunae@naver.com>
2025-04-10 09:35:45 +0900

    feat: home qa 수정

--- front/src/app/home/page.tsx
+++ front/src/app/home/page.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { useUserStore } from "@/stores/UserStore";
 import PageHeader from "../_components/common/PageHeader";
 import { RecommendationCard } from "./_components/RecommendationCard";
-import { KeywordMarquee } from "./_components/KeywordMarquee";
+import { Keyword } from "./_components/Keyword";
 import PopularNews from "./_components/PopularNews";
 import SearchBar from "../_components/common/SearchBar";
 
@@ -18,7 +18,7 @@ export default function HomePage() {
           </>
         }
       />
-      <div className="py-5">
+      <div className="pb-6 pt-6">
         <SearchBar />
       </div>
 
@@ -26,7 +26,7 @@ export default function HomePage() {
       <RecommendationCard />
 
       {/* 사용자 키워드 현황 */}
-      <KeywordMarquee />
+      <Keyword />
 
       {/* 인기 소식 */}
       <PopularNews />
